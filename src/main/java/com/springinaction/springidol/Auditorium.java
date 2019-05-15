d16a5a72f7cd1e1d7c9fcf17353cd57b6dcaa434
lesaoni <lesaoni@yandex.ru>
2019-05-15 18:35:30 +0300

    Contest enrichment

---
+++ src/main/java/com/springinaction/springidol/Auditorium.java
@@ -0,0 +1,11 @@
+package com.springinaction.springidol;
+
+public class Auditorium {
+
+    public void turnOnLights() {
+
+    }
+    public void turnOffLights() {
+
+    }
+}
---
+++ src/main/java/com/springinaction/springidol/Ticket.java
@@ -0,0 +1,4 @@
+package com.springinaction.springidol;
+
+public class Ticket {
+}
